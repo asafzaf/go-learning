d3d6d1f24bdcd39d1156f1aabd928c0901d2658c
asafzaf <asaf.zafrir@gmail.com>
2025-11-08 14:59:10 +0200

    added slice (array)

--- cards/main.go
+++ cards/main.go
@@ -5,25 +5,35 @@ import "fmt"
 func main() {
 
 	// First way to declare a variable
-	var card string = "Ace of Spades"
-	// Second way to declare a variable
-	card2 := "Two of Hearts"
-	// Re-assigning a variable
-	card2 = "Three of Diamonds"
+	// var card string = "Ace of Spades"
+	// // Second way to declare a variable
+	// card2 := "Two of Hearts"
+	// // Re-assigning a variable
+	// card2 = "Three of Diamonds"
 
-	var age int = 30
-	var price float64 = 19.99
-	var isAvailable bool = true
-	isAvailable = false
+	// var age int = 30
+	// var price float64 = 19.99
+	// var isAvailable bool = true
+	// isAvailable = false
 
-	card3 := drawCard()
-	fmt.Println(card3)
+	// card3 := drawCard()
+	// fmt.Println(card3)
 
-	fmt.Println(card)
-	fmt.Println(card2)
-	fmt.Println(age)
-	fmt.Println(price)
-	fmt.Println(isAvailable)
+	// fmt.Println(card)
+	// fmt.Println(card2)
+	// fmt.Println(age)
+	// fmt.Println(price)
+	// fmt.Println(isAvailable)
+
+	cardsArray := []string{"Ace of Spades", "Two of Hearts", "Three of Diamonds", drawCard()}
+
+	cardsArray = append(cardsArray, "Four of Clubs")
+
+	fmt.Println(cardsArray)
+
+	for i, card := range cardsArray {
+		fmt.Println(i, card)
+	}
 
 	printState()
 }
