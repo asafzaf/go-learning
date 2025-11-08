a558325d141650b7d7aceccc8b47f88f7e452656
asafzaf <asaf.zafrir@gmail.com>
2025-11-09 00:20:42 +0200

    receiver function

--- struct-section/main.go
+++ struct-section/main.go
@@ -27,16 +27,14 @@ func main() {
 			zipCode: 12345,
 		},
 		contactInfo: contactInfo{
-			email:   "john.doe@example.com",
-			zipCode: 12345,
+			email:   "john.doe222@example.com",
+			zipCode: 222222,
 		},
 	}
-	fmt.Println(p1)
-	p1.firstName = "Jane"
-	p1.lastName = "Smith"
-	p1.age = 28
-	fmt.Println(p1)
-	p1.age++
-	fmt.Println(p1)
-	fmt.Printf("%+v", p1)
+
+	p1.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
