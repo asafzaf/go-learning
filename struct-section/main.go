eb5f9683835d08ea313e2376521b64b7cf89d65d
asafzaf <asaf.zafrir@gmail.com>
2025-11-13 15:35:57 +0200

    added more example

--- struct-section/main.go
+++ struct-section/main.go
@@ -37,6 +37,20 @@ func main() {
 	// Alternatively, i can call the method directly on the struct instance
 	p1.updateName("Mike")
 	p1.print()
+
+	fmt.Println("new example")
+
+	str := "Hello, World!"
+	fmt.Println(str)
+	modifyString(str)
+	fmt.Println(str)
+	str2 := modifyString(str)
+	fmt.Println(str2)
+}
+
+func modifyString(s string) string {
+	s = "Modified String"
+	return s
 }
 
 func (pointerToPerson *person) updateName(newFirstName string) {
